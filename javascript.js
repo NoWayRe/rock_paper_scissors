63ee00b148410895e80dafc470b2591a9f8f8919
NoWayRe <78691199+NoWayRe@users.noreply.github.com>
2023-01-31 09:08:17 +0100

    Add function for computer player choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-console.log("Hi");
+const gameArray = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+    let choice = gameArray[Math.floor(Math.random() * gameArray.length)];
+    return choice;
+}
+
+console.log(getComputerChoice());
