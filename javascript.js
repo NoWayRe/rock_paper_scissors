533484314fe716fad23825d2d6ddca9370573b39
NoWayRe <78691199+NoWayRe@users.noreply.github.com>
2023-02-01 08:13:04 +0100

    Add player selection and game round function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,44 @@
 const gameArray = ["Rock", "Paper", "Scissors"];
 
+
+gameRound(playerSelection(), getComputerChoice());
+
+
 function getComputerChoice() {
-    let choice = gameArray[Math.floor(Math.random() * gameArray.length)];
+    let choice = gameArray[Math.floor(Math.random() * gameArray.length)].toUpperCase();
     return choice;
 }
 
-console.log(getComputerChoice());
+function playerSelection() {
+    let choice = prompt("Rock, paper, scissors, shoot!");
+    if (typeof(choice) != "string") {
+        console.log("invalid input. Please try again");
+    } else {
+        choice = choice.toUpperCase();
+    }
+    return choice;
+}
+
+//Plays one round of the game.
+
+function gameRound(playerChoice, computerChoice) {
+
+    if (playerChoice === computerChoice) {
+        console.log("It's a tie!");
+    }
+
+    else if (playerChoice === "ROCK" && computerChoice === "SCISSORS" || playerChoice === "SCISSORS" && computerChoice === "PAPER" || playerChoice === "PAPER" && computerChoice === "ROCK") {
+        console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
+    }
+
+    else if (computerChoice  === "ROCK" && playerChoice === "SCISSORS" || computerChoice  === "SCISSORS" && playerChoice === "PAPER" || computerChoice === "PAPER" && playerChoice === "ROCK") {
+        console.log(`You Lose! ${computerChoice} beats ${playerChoice}`);
+    }
+
+    else {
+        console.log(`Choice of "${playerChoice}" not valid. Try again next round!`);
+    }
+
+}
+
+
